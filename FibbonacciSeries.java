a965bd01699399d042cc09b538f831e86a2270c3
suzel <64069582+suzelkhan1177@users.noreply.github.com>
2022-04-27 10:42:23 -0700

    Update FibbonacciSeries.java

--- FibbonacciSeries.java
+++ FibbonacciSeries.java
@@ -1,6 +1,39 @@
 package questions2;
 import java.util.Scanner;
 public class FibbonacciSeries {
+	
+	//Recurtion
+	static int fib(int n) {
+		if( n <= 0 ) {
+			return 0;
+		}else if( n == 1 ) {
+			return 1;
+		}else {
+			return ( fib( n - 1 )+ fib( n - 2 ));
+		}
+	
+	// Check Fibbonachi or not
+	public static boolean checkMember(int n){
+				
+        if(n ==0 || n ==1){
+            return true;
+        }
+        
+        
+        int a=0, b=1;
+        
+        do{
+            int c = a;
+               a= b;
+              b = c+a;
+        }while(b < n);
+        
+        if(b ==n){
+            
+            return true;
+        }else{
+            return false;
+        }
 
 	public static void main(String[] args) {
 		
