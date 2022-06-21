b941b5d4a35ea15e82a51cf6a4b8b754e85ad3a5
suzel <64069582+suzelkhan1177@users.noreply.github.com>
2022-06-21 06:27:33 -0700

    Update CountWords.java

--- CountWords.java
+++ CountWords.java
@@ -9,7 +9,12 @@ public class CountWords {
 		
 		String s= sc.nextLine();
 		
-		int count=1;
+		 int count;
+		  if(str.isEmpty())
+			  count =0;
+		  else
+			 count=1;
+		
 		for(int i=0; i<s.length()-1; i++) {
 				
 			if((s.charAt(i)==' ' ) &&  (s.charAt(i+1) !=' ')) {
