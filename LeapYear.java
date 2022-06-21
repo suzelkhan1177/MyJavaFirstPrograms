0a7b6f4c0e4a2133cd1d003b3dafb84df1a5df51
suzel <64069582+suzelkhan1177@users.noreply.github.com>
2022-06-21 06:00:26 -0700

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,40 @@
+
+public class LeapYear{
+  
+	
+	 public static void main(String[] args) {
+		
+		 
+		 int year = 2012;
+		 
+		 boolean leap = false;
+		 
+		 if(year % 4 == 0) {
+			 leap = true;
+			 
+			 if(year % 100 == 0) {
+				 
+				 if( year % 400 == 0) {
+					 leap = true;
+				 }else {
+					 leap = false;
+				 }
+				 
+			 }else {
+				 leap = true;
+			 }
+			 
+		 }else {
+			 leap = false;
+		 }
+		 
+		 
+		 if(leap) {
+			 System.out.println(year + " year is Leap year");
+		 }else {
+			 System.out.println(year + " year is Not Leap year");
+		 }
+		 
+		 
+	}
+}
